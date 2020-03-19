fd9e18cef0a1bdd605a7b09214fd50b72e61a897
DenisBeliant <58740617+DenisBeliant@users.noreply.github.com>
2020-03-19 16:27:25 +0100

    All it's ok
    Tout est ok pour la page de gestion des films, un simple bug pour l'affichage des étoiles. A résoudre.

--- 1-decouvrir-DOM-jquery/niveau4/script.js
+++ 1-decouvrir-DOM-jquery/niveau4/script.js
@@ -1,25 +1,91 @@
 var listOfMovie = [];
 
 $(document).ready(function(){
-// A completer
+
+      // completer le code ici
+      $('#refresh').click(function() {
+
+        $('#list').empty();
+        listOfMovie = [];
+        
+        $.get('./playlist.txt', function(playlist) {
+           
+           playlist = splitFile(playlist);
+
+           listOfMovie.forEach(e => {
+             $('#list').append(htmlDivElement(e));
+
+             $('#'+e.index).click(function() {
+                  console.log(e.name);
+                  $('h1').html('Film en cours : '+e.name);
+                  $('#duree').html(e.length); 
+             });  
+
+           });
+        });
+
+  
+     });
+
 });
 
+// Fonction pour convertir la durée en nombre de secondes :
+function timeToDecimal(t) {
 
+  var arr = t.split('h');
+  var dec = parseInt((arr[1]/6)*10, 10);
+
+  return parseFloat(parseInt(arr[0], 10) + '.' + (dec<10?'0':'') + dec) * 60;
+
+}   
+
+// fonction de gestion de la barre de progression :
+function progress(value, max) {
+
+}
+
+function rank(star) {
+  
+  var html = '<i class="fas fa-star"></i>';
+  for(i = 0; i < star /2;i++) {
+    html += html;
+  }
+  return html;
+}
 
 function htmlDivElement(movie){
- // A completer
+
+  var html = "<div class='divFilm'><i class='fas fa-play underFilm' id='"+movie.index+"'></i><div class='divIndex'>"+movie.index+"</div><div class='divTitle'>"+movie.name+" durée : "+movie.length+"</div><div class='rank'>"+rank(movie.rank)+"</div></div>";
+// completer le code ici
+  return html;
+
 }
 
-function createPlayCallback(movie){
-// A completer
-}
- 
-function createMovie(i, n, d){
-// A completer
+function createMovie(i, n, d, r){
+
+  var movie = {
+    index: i,
+    name: n,
+    length: d,
+    rank: r
+  };
+
+// completer le code ici
+  return movie;
 }
 
-function splitFile(data){ 
-// A completer
+function splitFile(data){
+  // completer le code ici
+  data = data.split('\n');
+
+  data.forEach(e => {
+
+    movie = e.split(',');
+    console.log(movie);
+    addMovie(createMovie(movie[0], movie[1], movie[2], movie[3]));
+    
+  });
+  
 }
 
 function addMovie(m){
@@ -28,8 +94,9 @@ function addMovie(m){
 
 }
     
+function createPlayCallback(movie) {
 
-
+}
 
 
 
